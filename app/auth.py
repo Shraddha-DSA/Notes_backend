46072ae378855fd9dcc2b92bd67e4833c218b9b8
Shraddha Tiwari <shraddhatiwari345@gmail.com>
2026-06-29 19:36:10 +0530

    Improved code

--- app/auth.py
+++ app/auth.py
@@ -1,13 +1,12 @@
 from passlib.context import CryptContext
 from jose import JWTError, jwt
 from datetime import datetime, timedelta,timezone
+from app.config import (SECRET_KEY,ALGORITHM,ACCESS_TOKEN_EXPIRE_MINUTES)
 pwd_context=CryptContext(
     schemes=["bcrypt"],
     deprecated="auto"
 )
-SECRET_KEY="shraddha"
-ALGORITHM="HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES=30
+
 def hash_password(password: str):
     return pwd_context.hash(password)
 
